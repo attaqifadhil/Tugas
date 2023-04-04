71ef2db049160f8b62d7d9fb60924a7d740191a8
attaqifadhil <110644935+attaqifadhil@users.noreply.github.com>
2023-04-04 08:42:19 +0700

    Add files via upload

---
+++ TokoOnlinePolymorphisme/Main.java
@@ -0,0 +1,17 @@
+package TokoOnlinePolymorphisme;
+
+public class Main {
+    public static void main(String[] args) {
+        Barang barang = new Barang();
+        Karyawan karyawan = new Karyawan();
+        Laporan laporan = new Laporan();
+        Member member = new Member();
+        Transaksi transaksi = new Transaksi();
+
+
+        laporan.laporan(barang);
+        laporan.laporan(member);
+        laporan.laporan(transaksi, barang);
+
+    }
+}
